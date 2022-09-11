9cb473483e36418c9b55d32ac79c2e6600e076b0
Priya <pk3616182@gmail.com>
2022-09-11 13:24:35 +0530

    fixing register route, returnto behavior

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log("REQ.USER...", req.user);
-    if(!req.isAuthenticated()) {
-        req.flash('error', 'you must be signed in first!');
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
     next();
