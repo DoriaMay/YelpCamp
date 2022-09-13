766054dbfbdf9b965b1ea5f20f2a9b185d4934ba
Priya <pk3616182@gmail.com>
2022-09-13 12:25:03 +0530

    Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '631c2c01e0ee8b24b8f58bf0',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/1599413',
